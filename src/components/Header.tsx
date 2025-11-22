35c6ef9c204573c173ac704a69f55f20c4d88bdc
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-11-22 17:50:05 -0500

    …

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export default function SiteHeader(){
             <Flex vertical align="center">
                 <h1 className="roboto-slab title" style={{marginBottom:"1rem"}}>Malcolm X Ipsum</h1>
                 <Paragraph style={{fontStyle:"italic", fontSize:"1.2rem", marginBottom:"1rem"}}>The most radical lorem ipsum in history.</Paragraph>
-                <Paragraph className="roboto-slab para" style={{marginBottom:"1rem", textAlign:"center",maxWidth:"45ch"}}>How many paragraphs of revolutionary ideas do you need?</Paragraph>
+                <Paragraph className="roboto-slab para" style={{marginBottom:"1rem", textAlign:"center",maxWidth:"45ch"}}>How many paragraphs of revolutionary thought do you need?</Paragraph>
                 <StyledInputNumber style={{marginBottom:"1.5rem"}} min={1} max={5} defaultValue={5} changeOnWheel={true}/>
                 <Button type="primary" size="large">By Any Means Necessary</Button>
             </Flex>

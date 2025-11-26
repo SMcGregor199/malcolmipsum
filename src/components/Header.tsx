f760f30900f6cb13af54241f01bc4b55f3414fb9
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-11-26 16:02:58 -0500

    feat: complete generateParagraphs function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
 import {Layout, Typography, Flex, Button} from "antd";
+import { useState } from "react";
 import styled from "@emotion/styled"
 import { InputNumber } from 'antd';
 const {Paragraph} = Typography;
 const {Header} = Layout;
+
+type SiteHeaderProps = {
+  onGenerate: (count: number) => void;
+};
 
 const StyledInputNumber = styled(InputNumber)({
     padding:"10px 20px",
@@ -18,7 +23,25 @@ const StyledDiv = styled.div({
     boxShadow:"0 8px 24px rgba(0, 0, 0, 0.12)",
     padding:"1.5rem"
 })
-export default function SiteHeader(){
+export default function SiteHeader(props: SiteHeaderProps){
+    const {onGenerate} = props;
+    const [inputValue, setInputValue] = useState<number>(5);
+
+    // const handleChange = (value: number | string | null) => {
+    //     if (typeof value === "number") {
+    //         setInputValue(value);  
+    //     }
+    //   };
+    const handleChange = (value: number | string | null) => {
+        if (typeof value === "number") {
+            setInputValue(value);  
+        }
+    }
+    const handleClick = () => {
+        if (inputValue > 0) {
+            onGenerate(inputValue);
+        }
+    };
     return(
         <Header style={{height:"min-content", marginBottom:"2.5rem"}}>
             <Flex vertical align="center">
@@ -26,8 +49,8 @@ export default function SiteHeader(){
                 <Paragraph style={{fontSize:"1rem", marginBottom:"2.5rem", color:"#4B4F56"}}>"The future belongs to those who prepare for it today."</Paragraph>
                 <StyledDiv>
                     <Paragraph className="roboto-slab para" style={{marginBottom:"1rem", textAlign:"center"}}>How many paragraphs of powerful oratory do you need?</Paragraph>
-                    <StyledInputNumber style={{marginBottom:"1.5rem", display:"block", backgroundColor:"var(--primary-color)"}} min={1} max={5} defaultValue={5} changeOnWheel={true}/>
-                    <Button type="primary" size="large">GENERATE TEXT</Button>
+                    <StyledInputNumber onChange={handleChange} onPressEnter={handleClick} style={{marginBottom:"1.5rem", display:"block", backgroundColor:"var(--primary-color)"}} min={1} max={5} defaultValue={5} changeOnWheel={true}/>
+                    <Button type="primary" size="large" onClick={handleClick}>GENERATE TEXT</Button>
                 </StyledDiv>
             </Flex>
         </Header>
